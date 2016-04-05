ccb37edebe1dbdd6cac3337fba9487237130abef
kennyhsu0721 <kenny0721@msn.com>
2016-04-05 23:20:18 +0800

    加入專案檔案。

---
+++ Web/Ir.Web.UI/Areas/Lab/Controllers/Lab1Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ir.Web.UI.Areas.Lab1.Controllers
+{
+    [Area("Lab")]
+    public class Lab1Controller : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Web/Ir.Web.UI/wwwroot/apps/lab/lab1/lab1.c.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('irapp')
+        .controller('lab01c', lab01c);
+
+    lab01c.$inject = ['$scope'];
+    
+    function lab01c($scope) {
+        $scope.title = 'lab01c';
+
+        activate();
+
+        function activate() { }
+    }
+})();
